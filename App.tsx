9e5164d824c89a2361b7ab7d4e872563f3399592
EunhaLim <dmsgk1559@gmail.com>
2023-06-09 21:22:26 +0900

    키보드 설정 2 - 키보드 외 영역 클릭시 키보드 내리기

--- App.tsx
+++ App.tsx
@@ -1,32 +1,43 @@
-import {useState} from 'react';
+import {PropsWithChildren, useState} from 'react';
 import {
   Button,
+  Keyboard,
   KeyboardAvoidingView,
   Platform,
   StyleSheet,
   Text,
+  TouchableWithoutFeedback,
   View,
 } from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import Header from './components/Header';
 import NameInput from './components/NameInput';
 
+const DismissKeyboard = ({children}: PropsWithChildren) => (
+  <TouchableWithoutFeedback
+    style={styles.block}
+    onPress={() => Keyboard.dismiss()}>
+    {children}
+  </TouchableWithoutFeedback>
+);
 const App = () => {
   const [name, setName] = useState('');
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.block} edges={['bottom']}>
-        <KeyboardAvoidingView
-          style={styles.block}
-          behavior={Platform.select({ios: 'padding'})}>
-          <Header />
-          <View style={styles.content}>
-            <Text style={styles.text}>TEST</Text>
-            <Button title='Hello' />
-          </View>
-          <NameInput name={name} changeName={setName} />
-        </KeyboardAvoidingView>
+        <DismissKeyboard>
+          <KeyboardAvoidingView
+            style={styles.block}
+            behavior={Platform.select({ios: 'padding'})}>
+            <Header />
+            <View style={styles.content}>
+              <Text style={styles.text}>TEST</Text>
+              <Button title='Hello' />
+            </View>
+            <NameInput name={name} changeName={setName} />
+          </KeyboardAvoidingView>
+        </DismissKeyboard>
       </SafeAreaView>
     </SafeAreaProvider>
   );
