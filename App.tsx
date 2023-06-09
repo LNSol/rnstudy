28d942137ae6bd4c25645f92746277126a8147ba
EunhaLim <dmsgk1559@gmail.com>
2023-06-09 21:17:49 +0900

    ios 환경-키보드가 나타난 경우 하단 내용 보이게 설정

--- App.tsx
+++ App.tsx
@@ -1,15 +1,48 @@
-import {Text} from 'react-native';
-import Header from './components/Header';
+import {useState} from 'react';
+import {
+  Button,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import Header from './components/Header';
+import NameInput from './components/NameInput';
 
 const App = () => {
+  const [name, setName] = useState('');
+
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
-        <Header />
-        <Text>리액트 네이티브 스터디</Text>
+      <SafeAreaView style={styles.block} edges={['bottom']}>
+        <KeyboardAvoidingView
+          style={styles.block}
+          behavior={Platform.select({ios: 'padding'})}>
+          <Header />
+          <View style={styles.content}>
+            <Text style={styles.text}>TEST</Text>
+            <Button title='Hello' />
+          </View>
+          <NameInput name={name} changeName={setName} />
+        </KeyboardAvoidingView>
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 30,
+  },
+});
 export default App;
--- components/NameInput.tsx
+++ components/NameInput.tsx
@@ -1,14 +1,35 @@
-import {StyleSheet, View, Text} from 'react-native';
+import {Dispatch, SetStateAction} from 'react';
+import {StyleSheet, View, TextInput} from 'react-native';
 
-const NameInput = () => {
+interface INameInputProps {
+  name: string;
+  changeName: Dispatch<SetStateAction<string>>;
+}
+
+const NameInput = ({name, changeName}: INameInputProps) => {
   return (
     <View style={styles.block}>
-      <Text>NameInput</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={changeName}
+        placeholder='이름을 입력하세요.'
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  block: {},
+  block: {
+    height: 60,
+    paddingHorizontal: 16,
+    justifyContent: 'center',
+    borderColor: '#8292aa',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+  input: {
+    fontSize: 16,
+  },
 });
 export default NameInput;
