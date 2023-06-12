fb5556edbace42d582bde3379bed9b6b2998eda1
EunhaLim <dmsgk1559@gmail.com>
2023-06-12 12:40:02 +0900

    버튼 만들기 - TouchableOpacity

--- components/NameInput.tsx
+++ components/NameInput.tsx
@@ -1,5 +1,6 @@
 import {Dispatch, SetStateAction, useEffect, useRef} from 'react';
-import {StyleSheet, View, TextInput} from 'react-native';
+import {StyleSheet, View, TextInput, TouchableOpacity} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 interface INameInputProps {
   name: string;
@@ -25,6 +26,11 @@ const NameInput = ({name, changeName, focus, greeting}: INameInputProps) => {
         ref={inputRef}
         onSubmitEditing={greeting}
       />
+      <View style={styles.button}>
+        <TouchableOpacity style={styles.add} activeOpacity={0.5}>
+          <Icon name='add' color='#fff' size={24} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -33,13 +39,30 @@ const styles = StyleSheet.create({
   block: {
     height: 60,
     paddingHorizontal: 16,
-    justifyContent: 'center',
     borderColor: '#8292aa',
     borderTopWidth: 1,
     borderBottomWidth: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
   input: {
     fontSize: 16,
+    flex: 1,
+  },
+  button: {
+    width: 46,
+    height: 46,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 23,
+  },
+  add: {
+    width: 46,
+    height: 46,
+    borderRadius: 23,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#504aed',
   },
 });
 export default NameInput;
